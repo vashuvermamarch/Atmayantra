21ba0f76606d92b08a5f20fa70f8f18431a94745
vashuvermamarch <karanverma24march@gmail.com>
2025-10-20 22:09:03 +0530

    refactor(settings): Configure project for deployment
    This commit modifies the settings to use environment variables for secrets and configuration, preparing the project for a secure deployment.

--- Atmayantra/Atmayantra/settings.py
+++ Atmayantra/Atmayantra/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 from pathlib import Path
 import os
 import dj_database_url
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-1bnpnm#%l77+8!zjjf&6en9nmf8#y_++qj@3^ch=h$%9t!*u5a')
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('RENDER', 'False').lower() != 'true'
+DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
 ALLOWED_HOSTS = []
 
