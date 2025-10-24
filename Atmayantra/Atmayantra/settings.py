68655047cebc0a4ca6b2ac73d6ee85c35132358a
vashuvermamarch <karanverma24march@gmail.com>
2025-10-24 22:12:58 +0530

    fix: Update ALLOWED_HOSTS for Render deployment

--- Atmayantra/Atmayantra/settings.py
+++ Atmayantra/Atmayantra/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 
 RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
 if RENDER_EXTERNAL_HOSTNAME:
