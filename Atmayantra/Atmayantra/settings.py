e5126c7fb0aa5db17f13ba9e0bcace749c715ec3
vashuvermamarch <karanverma24march@gmail.com>
2025-10-24 22:49:21 +0530

    fix: Update ALLOWED_HOSTS for Render deployment

--- Atmayantra/Atmayantra/settings.py
+++ Atmayantra/Atmayantra/settings.py
@@ -98,11 +98,11 @@ WSGI_APPLICATION = 'Atmayantra.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=f"sqlite:///{BASE_DIR / 'db.sqlite3'}"
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
