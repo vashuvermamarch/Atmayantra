5676278894f0ab9de6ed27413f548f902fae5bdf
vashuvermamarch <karanverma24march@gmail.com>
2025-10-25 14:03:54 +0530

    feat: Configure dynamic database for Render deployment

--- Atmayantra/Atmayantra/settings.py
+++ Atmayantra/Atmayantra/settings.py
@@ -98,10 +98,11 @@ WSGI_APPLICATION = 'Atmayantra.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///{}'.format(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 # Password validation
