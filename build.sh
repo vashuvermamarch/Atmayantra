3832d1c3db9745c885b7e460620c47fba95f9dec
vashuvermamarch <karanverma24march@gmail.com>
2025-11-15 14:59:09 +0530

    Feat: Refactor URLs, permissions, and add deployment configs

--- build.sh
+++ build.sh
@@ -5,5 +5,10 @@ set -o errexit
 pip install --upgrade pip
 pip install -r requirements.txt
 
-python Atmayantra/manage.py collectstatic --no-input
-python Atmayantra/manage.py migrate
+python Atmayantra/manage.py collectstatic --no-input --clear
+
+# Run migrations, but if it fails, fake the problematic migration and try again.
+# This is a common fix for inconsistent migration history during deployment.
+python Atmayantra/manage.py migrate || ( \
+  python Atmayantra/manage.py migrate --fake doctor_bank_details 0002_remove_doctorbankdetails_profile_photo_and_more && \
+  python Atmayantra/manage.py migrate )
