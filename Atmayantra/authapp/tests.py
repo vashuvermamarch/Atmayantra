58f6fbb28664335645f84c798ce5be10cd4c51be
vashuvermamarch <karanverma24march@gmail.com>
2026-05-26 16:48:52 +0530

    Trigger fresh CI/CD run

--- Atmayantra/authapp/tests.py
+++ Atmayantra/authapp/tests.py
@@ -2,6 +2,7 @@ from django.contrib.auth import get_user_model
 from django.test import TestCase
 
 
+# Trigger new CI/CD workflow run
 class UserModelTests(TestCase):
     def test_create_user(self):
         User = get_user_model()
